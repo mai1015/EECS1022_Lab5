49386facbb53933bc1abbdd3021c706736255c9f
mai1015 <i@mai1015.com>
2018-03-01 19:46:46 -0500

    Initial commit

---
+++ app/src/main/java/eecs1022/lab5/Lab5Activity.java
@@ -0,0 +1,15 @@
+package eecs1022.lab5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab5Activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab5);
+    }
+}
